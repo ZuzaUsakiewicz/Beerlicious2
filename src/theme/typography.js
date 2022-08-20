26ebf891e454bb9063727dc02dc7c522cede78fa
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-20 22:08:31 +0200

    add basic typography properties

---
+++ src/theme/typography.js
@@ -0,0 +1,18 @@
+export const typography = {
+  // font sizes:
+  smallFontSize: "0.5rem",
+  regularFontSize: "1rem",
+  mediumFontSize: "1.2rem",
+  largeFontSize: "1.5rem",
+  xtraLargeFontSize: "3rem",
+
+  // letter-spacing:
+  smallLetterSpace: "2px",
+
+  //  font weights:
+  thinFont: 300,
+  regularFont: 400,
+  mediumFont: 500,
+  boldFont: 600,
+  veryBoldFont: 700,
+};
